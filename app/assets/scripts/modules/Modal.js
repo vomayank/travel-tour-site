cbaaed9af31de75dd6c310865b46c6f7120d6e96
Mayank Rai <gavkadon@gmail.com>
2018-02-06 19:03:37 +0530

    Completed the create modla

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,39 @@
+import $ from 'jQuery';
+
+class Modal {
+    constructor() {
+        this.openModalButton = $(".open-modal");
+        this.modal = $(".modal");
+        this.closeModalButton = $(".modal__close");
+        this.event();
+    }
+
+    event() {
+        // clicking the open modal button
+        this.openModalButton.click(this.openModal.bind(this));
+
+        // clciking the X to close modal button
+        this.closeModalButton.click(this.closeModal.bind(this));
+
+        // pushes any key
+        $(document).keyup(this.keyPressHandler.bind(this));
+
+    }
+
+    keyPressHandler(e) {
+        if (e.keyCode == 27) {
+            this.closeModal();
+        }
+    }
+
+    openModal() {
+        this.modal.addClass("modal--is-visible");
+        return false;
+    }
+
+    closeModal() {
+        this.modal.removeClass("modal--is-visible");
+    }
+}
+
+export default Modal;
